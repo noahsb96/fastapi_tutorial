286fa31f4d03fb823a29942e93972b1924f1ee6c
noahsb96 <noahsb96@yahoo.com>
2024-03-10 23:15:52 -0600

    Added type hints to parameters. Now autocomplete works for .title

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def get_full_name(first_name, last_name):
+def get_full_name(first_name: str, last_name: str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
-
 
 print(get_full_name("john", "doe"))
 
@@ -10,3 +9,15 @@ print(get_full_name("john", "doe"))
 # Takes a first_name and last_name.
 # Converts the first letter of each one to upper case with title().
 # Concatenates them with a space in the middle.
+
+# It's a different thing.
+
+# We are using colons (:), not equals (=).
+
+# And adding type hints normally doesn't change what happens from what would happen without them.
+
+# But now, imagine you are again in the middle of creating that function, but with type hints.
+
+# At the same point, you try to trigger the autocomplete with Ctrl+Space and you see:
+
+# With that, you can scroll, seeing the options, until you find the one that "rings a bell":
