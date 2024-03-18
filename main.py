44211b078862c2a0f1a84eb1d02909b6e4ab203d
noahsb96 <noahsb96@yahoo.com>
2024-03-17 22:17:24 -0600

    Received JSON response as int and not string

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 async def read_item(item_id: int): # this function as the argument item_id
     return {"item_id": item_id} # run this function, start uvicorn and go to http://127.0.0.1:8000/items/foo to see response
 # returns {"item_id": "foo"}
+
+# if you run this example at http://127.0.0.1:8000/items/3
+# returns {"item_id": 3}
