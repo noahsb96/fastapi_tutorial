cff47a797002c66040d945a6ec03b3650df0a251
noahsb96 <noahsb96@yahoo.com>
2024-03-17 21:54:50 -0600

    Can add type to argument to declare path parameter type

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/items/{item_id}") # value of path parameter item_id will be passed to 
-async def read_item(item_id): # this function as the argument item_id
+async def read_item(item_id: int): # this function as the argument item_id
     return {"item_id": item_id} # run this function, start uvicorn and go to http://127.0.0.1:8000/items/foo to see response
 # returns {"item_id": "foo"}
