51ad58dffd77472926cf15a3937103678a9c05cc
noahsb96 <noahsb96@yahoo.com>
2024-04-04 22:48:39 -0600

    adding annotated and query to parameters

--- main.py
+++ main.py
@@ -231,3 +231,16 @@ async def read_items(q: str | None = None):
 # The query parameter q is of type Union[str, None] (or str | None in Python 3.10), that means that it's of type str but could also be None, and indeed, the default value is None, so FastAPI will know it's not required.
 
 # We are going to enforce that even though q is optional, whenever it is provided, its length doesn't exceed 50 characters
+
+# To achieve this, we have to import 2 things:
+from fastapi import Query
+from typing import Annotated
+
+# In Python 3.9 or above, Annotated is part of the standard library, so you can import it from typing
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
