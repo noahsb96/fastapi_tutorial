55dbaba1ff26b92d35f36af2a05a4cfb8cff5bef
noahsb96 <noahsb96@yahoo.com>
2024-04-17 23:18:46 -0600

    Added a title and description to q parameter

--- main.py
+++ main.py
@@ -375,7 +375,25 @@ from typing import Annotated
 # The default of q will be ["foo", "bar"] and the response would be the same as above
 
 # You can also just write list instead of just using List[str] or list[str] in Python 3.9+
+# @app.get("/items/")
+# async def read_items(q: Annotated[list, Query()] = []):
+#     query_items = {"q": q}
+#     return query_items
+
+# We can add more info to the parameter
+# We can add a title and a description
 @app.get("/items/")
-async def read_items(q: Annotated[list, Query()] = []):
-    query_items = {"q": q}
-    return query_items
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
